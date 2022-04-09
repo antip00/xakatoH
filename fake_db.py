4988206e9b2216a237c490d394509b2a19469b8d
Hova Feklisova <magnificent.hova@gmail.com>
2022-04-10 01:43:59 +0300

    R/W to DB

--- fake_db.py
+++ fake_db.py
@@ -4,7 +4,7 @@ import pandas as pd
 dates = ["2022-05-%02d" % i for i in range(1, 31)]
 
 # https://codegolf.stackexchange.com/questions/49728/list-all-times-in-the-day-at-a-half-hour-rate
-periods = ["'%02d:%s0'" % (h / 2, h % 2 * 3) for h in range(20, 38)]
+periods = ["%02d:%s0" % (h / 2, h % 2 * 3) for h in range(20, 38)]
 
 df = pd.DataFrame(list(product(range(4), dates, periods, [None])), columns=["room_name", "date", "time_id", "user"])
 df.to_pickle("db.pkl")
--- knopka_server.py
+++ knopka_server.py
@@ -1,7 +1,10 @@
 import json
+import pandas as pd
 import base64
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+
+db = pd.read_pickle("db.pkl")
 
 app = FastAPI()
 
@@ -24,3 +27,47 @@ async def app_get(request: Request):
     body = await request.json()
     print(body)
     return {"test": "mew"}
+
+@app.post("/book")
+async def book_post(request: Request):
+    body = await request.json()
+    print(type(body), body)
+
+    contains_room_name = db["room_name"].str.contains(body["room_name"])
+    contains_date = db["date"].str.contains(body["date"])
+    contains_time_id = db["time_id"].str.contains(body["time_id"])
+
+    if not (contains_room_name.any() and \
+            contains_date.any() and \
+            contains_time_id.any()):
+
+        return { "success": False, "error_msg": "Incorrect data entry" }
+
+    # print(db)
+    db.loc[contains_room_name & contains_date & contains_time_id, "user"] = body["user"]
+    # print((contains_room_name & contains_date & contains_time_id).any())
+    # print(db)
+    return { "success": True, "error_msg": "Successfully booked the room" }
+
+@app.post("/unbook")
+async def unbook_post(request: Request):
+    body = await request.json()
+    print(type(body), body)
+
+    contains_room_name = db["room_name"].str.contains(body["room_name"])
+    contains_date = db["date"].str.contains(body["date"])
+    contains_time_id = db["time_id"].str.contains(body["time_id"])
+
+    if not (contains_room_name.any() and \
+            contains_date.any() and \
+            contains_time_id.any()):
+
+        return { "success": False, "error_msg": "Incorrect data entry" }
+
+    db.loc[contains_room_name & contains_date & contains_time_id, "user"] = None
+    # print((contains_room_name & contains_date & contains_time_id).any())
+    return { "success": True, "error_msg": "Successfully unbooked the room" }
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    db.to_pickle("db.pkl")
